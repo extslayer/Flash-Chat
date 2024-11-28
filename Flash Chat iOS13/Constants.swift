4216dc4022cbb0ab3a4d2b429b71137c2cada698
extslayer <kabirismanmohan@gmail.com>
2024-11-28 22:52:13 +0530

    created a custom cell for the table

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Manmohan Shrivastava on 27/11/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Manmohan Shrivastava on 27/11/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
