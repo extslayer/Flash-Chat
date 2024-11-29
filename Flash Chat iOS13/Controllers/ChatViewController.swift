74697bff3b7823f14505adb9e00c17706741a6a2
extslayer <kabirismanmohan@gmail.com>
2024-11-29 14:39:19 +0530

    created a database in firestore and successfully added messaged in it

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -17,6 +17,8 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
     var message : [Message] = [Message(sender: "1@2.com", body: "Hey there!"),
                                Message(sender: "manmohan@gmail.com", body: "Kesa hai bhai"),
                                Message(sender: "3@4.com", body: "Me badhiya tu bata")]
@@ -32,6 +34,20 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody
+            ]) { error in
+                if let e = error{
+                    print("there was an issue with firestore")
+                    print(e)
+                } else{
+                    print("successly saved the data to firestore")
+                }
+            }
+            
+        }
     }
     
     @IBAction func logOutButtonPressed(_ sender: UIBarButtonItem) {
